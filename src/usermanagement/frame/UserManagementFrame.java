0fd266434370378c395a08f85fa0bff83fb64646
smj0104 <hikoseki1997@naver.com>
2023-01-26 16:47:36 +0900

    0126 로그인 수정

--- src/usermanagement/frame/UserManagementFrame.java
+++ src/usermanagement/frame/UserManagementFrame.java
@@ -1,4 +1,4 @@
-package usermanagement.frame; //테스팅은 최대한 많이 끝까지해볼것
+package usermanagement.frame;
 
 import java.awt.CardLayout;
 import java.awt.Color;
@@ -23,14 +23,12 @@ import javax.swing.SwingConstants;
 import com.google.gson.JsonObject;
 
 import usermanagement.service.UserService;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class UserManagementFrame extends JFrame {
 	
 	private List<JTextField> loginFields;
 	private List<JTextField> registerFields;
-
+	
 	private CardLayout mainCard;
 	private JPanel mainPanel;
 	private JTextField usernameField;
@@ -44,7 +42,7 @@ public class UserManagementFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UserManagementFrame frame = new UserManagementFrame();  //생성자 호출
+					UserManagementFrame frame = new UserManagementFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -64,54 +62,56 @@ public class UserManagementFrame extends JFrame {
 		mainPanel.setBorder(null);
 
 		setContentPane(mainPanel);
-
+		
 		mainCard = new CardLayout();
 		mainPanel.setLayout(mainCard);
-
+		
 		JPanel loginPanel = new JPanel();
 		loginPanel.setBackground(new Color(255, 255, 255));
 
 		mainPanel.add(loginPanel, "loginPanel");
 		loginPanel.setLayout(null);
-
+		
 		JLabel logoText = new JLabel("UserManagement");
 		logoText.setFont(new Font("CookieRun Regular", Font.BOLD, 20));
 		logoText.setHorizontalAlignment(SwingConstants.CENTER);
 		logoText.setBounds(86, 34, 213, 56);
 		loginPanel.add(logoText);
-
+		
 		JLabel loginText = new JLabel("Login");
 		loginText.setFont(new Font("Gadugi", Font.PLAIN, 30));
 		loginText.setHorizontalAlignment(SwingConstants.CENTER);
 		loginText.setBounds(121, 69, 135, 56);
 		loginPanel.add(loginText);
-
+		
 		usernameField = new JTextField();
 		usernameField.setHorizontalAlignment(SwingConstants.CENTER);
 		usernameField.setBounds(37, 210, 311, 26);
 		loginPanel.add(usernameField);
 		usernameField.setColumns(10);
-
+		
 		passwordField = new JPasswordField();
 		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
 		passwordField.setBounds(37, 261, 311, 26);
 		loginPanel.add(passwordField);
-
+		
 		JLabel usernameLabel = new JLabel("Username or email");
 		usernameLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		usernameLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		usernameLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		usernameLabel.setBounds(37, 194, 144, 15);
 		loginPanel.add(usernameLabel);
-
+		
 		JLabel passwordLabel = new JLabel("Password");
 		passwordLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		passwordLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		passwordLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		passwordLabel.setBounds(37, 246, 144, 15);
 		loginPanel.add(passwordLabel);
-
+		
 		JButton loginButton = new JButton("Login");
-		loginButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		
+		loginButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 				JsonObject loginUser = new JsonObject();
 				loginUser.addProperty("usernameAndEmail", usernameField.getText());
 				loginUser.addProperty("password", passwordField.getText());
@@ -120,7 +120,7 @@ public class UserManagementFrame extends JFrame {
 				
 				Map<String, String> response = userService.authorize(loginUser.toString());
 				
-				if(response.containsKey("ok")) {
+				if(response.containsKey("error")) {
 					JOptionPane.showMessageDialog(null, response.get("error"), "error", JOptionPane.ERROR_MESSAGE);
 					return;
 				}
@@ -130,33 +130,17 @@ public class UserManagementFrame extends JFrame {
 			}
 		});
 		
-//		MouseListener listener = new MouseAdapter() {
-//			@Override
-//			public void mouseClicked(MouseEvent e) {
-//				// TODO Auto-generated method stub
-//				super.mouseClicked(e);
-//			
-//		};
-//		
-//		loginButton.addMouseListener(listener);
-//		
-		loginButton.addMouseListener(new MouseAdapter() {  //마우스어댑터가 리스너를 이미 구현함 마우스 어댑터로 구현하고싶은것만 구현
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
 		loginButton.setForeground(new Color(0, 0, 0));
-		loginButton.setFont(new Font("CookieRun Black", Font.BOLD, 15));
+		loginButton.setFont(new Font("Gadugi", Font.BOLD, 15));
 		loginButton.setBackground(new Color(255, 255, 255));
 		loginButton.setBounds(37, 311, 311, 36);
 		loginPanel.add(loginButton);
-
+		
 		JLabel signupDesc = new JLabel("Don't have an account?");
-		signupDesc.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		signupDesc.setFont(new Font("Consolas", Font.PLAIN, 12));
 		signupDesc.setBounds(79, 374, 154, 26);
 		loginPanel.add(signupDesc);
-
+		
 		JLabel signupLink = new JLabel("Sign up");
 		signupLink.addMouseListener(new MouseAdapter() {
 			@Override
@@ -166,21 +150,21 @@ public class UserManagementFrame extends JFrame {
 			}
 		});
 		signupLink.setForeground(new Color(0, 102, 204));
-		signupLink.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		signupLink.setFont(new Font("Consolas", Font.PLAIN, 12));
 		signupLink.setBounds(242, 380, 57, 15);
 		loginPanel.add(signupLink);
-
+		
 		JLabel forgotPasswordLink = new JLabel("Forgot your password?");
 		forgotPasswordLink.setForeground(new Color(0, 102, 204));
-		forgotPasswordLink.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		forgotPasswordLink.setFont(new Font("Consolas", Font.PLAIN, 12));
 		forgotPasswordLink.setBounds(110, 394, 154, 26);
 		loginPanel.add(forgotPasswordLink);
-
+		
 		JPanel registerPanel = new JPanel();
 		registerPanel.setBackground(new Color(255, 255, 255));
 		mainPanel.add(registerPanel, "registerPanel");
 		registerPanel.setLayout(null);
-
+		
 		JLabel signinLink = new JLabel("Sign in");
 		signinLink.addMouseListener(new MouseAdapter() {
 			@Override
@@ -193,98 +177,97 @@ public class UserManagementFrame extends JFrame {
 		signinLink.setHorizontalAlignment(SwingConstants.CENTER);
 		signinLink.setBounds(236, 408, 111, 24);
 		registerPanel.add(signinLink);
-
+		
 		JLabel registerLogoText = new JLabel("UserManagement");
 		registerLogoText.setHorizontalAlignment(SwingConstants.CENTER);
 		registerLogoText.setFont(new Font("CookieRun Regular", Font.BOLD, 20));
 		registerLogoText.setBounds(86, 34, 213, 56);
 		registerPanel.add(registerLogoText);
-
+		
 		JLabel registerText = new JLabel("Register");
 		registerText.setHorizontalAlignment(SwingConstants.CENTER);
 		registerText.setFont(new Font("Gadugi", Font.PLAIN, 30));
 		registerText.setBounds(121, 69, 135, 56);
 		registerPanel.add(registerText);
-
+		
 		JLabel registerUsernameLabel = new JLabel("Username");
 		registerUsernameLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		registerUsernameLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		registerUsernameLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		registerUsernameLabel.setBounds(36, 135, 144, 15);
 		registerPanel.add(registerUsernameLabel);
-
+		
 		registerUsernameField = new JTextField();
 		registerUsernameField.setHorizontalAlignment(SwingConstants.CENTER);
 		registerUsernameField.setColumns(10);
 		registerUsernameField.setBounds(36, 151, 311, 26);
 		registerPanel.add(registerUsernameField);
-
+		
 		JLabel registerPasswordLabel = new JLabel("Password");
 		registerPasswordLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		registerPasswordLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		registerPasswordLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		registerPasswordLabel.setBounds(36, 187, 144, 15);
 		registerPanel.add(registerPasswordLabel);
-
+		
 		registerPasswordField = new JPasswordField();
 		registerPasswordField.setHorizontalAlignment(SwingConstants.CENTER);
 		registerPasswordField.setBounds(36, 202, 311, 26);
 		registerPanel.add(registerPasswordField);
-
+		
 		JLabel registerNameLabel = new JLabel("name");
 		registerNameLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		registerNameLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		registerNameLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		registerNameLabel.setBounds(36, 238, 144, 15);
 		registerPanel.add(registerNameLabel);
-
+		
 		registerNameField = new JTextField();
 		registerNameField.setHorizontalAlignment(SwingConstants.CENTER);
 		registerNameField.setColumns(10);
 		registerNameField.setBounds(36, 254, 311, 26);
 		registerPanel.add(registerNameField);
-
+		
 		JLabel registerEmailLabel = new JLabel("email");
 		registerEmailLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		registerEmailLabel.setFont(new Font("CookieRun Regular", Font.PLAIN, 12));
+		registerEmailLabel.setFont(new Font("D2Coding", Font.PLAIN, 12));
 		registerEmailLabel.setBounds(36, 290, 144, 15);
 		registerPanel.add(registerEmailLabel);
-
+		
 		registerEmailField = new JTextField();
 		registerEmailField.setHorizontalAlignment(SwingConstants.CENTER);
 		registerEmailField.setColumns(10);
 		registerEmailField.setBounds(36, 306, 311, 26);
 		registerPanel.add(registerEmailField);
-
-
-		JButton lblNewLabel_1 = new JButton("REGISTER");
-		lblNewLabel_1.addMouseListener(new MouseAdapter() {
-
-			public void mouseClicked(MouseEvent e) {  //정의만 한것 
+		
+		JButton registerButton = new JButton("Register");
+		
+		registerButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 				JsonObject userJson = new JsonObject();
 				userJson.addProperty("username", registerUsernameField.getText());
 				userJson.addProperty("password", registerPasswordField.getText());
 				userJson.addProperty("name", registerNameField.getText());
 				userJson.addProperty("email", registerEmailField.getText());
-
+				
 				UserService userService = UserService.getInstance();
-
+				
 				Map<String, String> response = userService.register(userJson.toString());
-
-				if (response.containsKey("error")) {
-					// 키값안에 들어있는지 확인해줌
-					JOptionPane.showMessageDialog(null, response.get("error"), "error", JOptionPane.ERROR_MESSAGE);
-					return; // 이벤트에 대한 리턴
+				
+				if(response.containsKey("error")) {
+					JOptionPane.showMessageDialog(null, response.get("error"), "error", JOptionPane.ERROR_MESSAGE);					
+					return;
 				}
 				
-				JOptionPane.showMessageDialog(null, response.get("확인"), "확인", JOptionPane.INFORMATION_MESSAGE);
-				mainCard.show(mainPanel, "loginPanel");  //화면 넘어감
-				clearFields(registerFields); //넘어가고 화면 클리어 , 클릭이 되었을때 호출됨 , 생성자가 먼저 호출되어서 값이 이미 들어가있음, 화면전환이 일어날때마다 해줄것
-
-				// userService.register(userJson.toString());
+				JOptionPane.showMessageDialog(null, response.get("ok"), "ok", JOptionPane.INFORMATION_MESSAGE);
+				mainCard.show(mainPanel, "loginPanel");
+				clearFields(registerFields);
 			}
 		});
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("CookieRun Black", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(93, 370, 206, 37);
-		registerPanel.add(lblNewLabel_1);
+		
+		registerButton.setForeground(Color.BLACK);
+		registerButton.setFont(new Font("Gadugi", Font.BOLD, 15));
+		registerButton.setBackground(Color.WHITE);
+		registerButton.setBounds(36, 353, 311, 36);
+		registerPanel.add(registerButton);
 		
 		loginFields.add(usernameField);
 		loginFields.add(passwordField);
@@ -293,16 +276,30 @@ public class UserManagementFrame extends JFrame {
 		registerFields.add(registerPasswordField);
 		registerFields.add(registerNameField);
 		registerFields.add(registerEmailField);
-	}  //생성자 호출이 여기까지 실행
+	}
 	
-	private void clearFields(List<JTextField> textFields) {  
+	private void clearFields(List<JTextField> textFields) {
 		for(JTextField field : textFields) {
-			if(field.getText().isEmpty()) {  //empty 스페이스바도 공백으로 본다
+			if(field.getText().isEmpty()) {
 				continue;
 			}
 			field.setText("");
 		}
-		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/usermanagement/service/UserService.java
+++ src/usermanagement/service/UserService.java
@@ -1,5 +1,5 @@
 package usermanagement.service;
-//ctrl shift o 정리 기억
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -37,14 +37,13 @@ public class UserService {
 		Map<String, String> response = new HashMap<>();
 
 		Map<String, String> userMap = gson.fromJson(userJson, Map.class);
+
 		for (Entry<String, String> userEntry : userMap.entrySet()) {
-			if (userEntry.getValue().isBlank()) { // getValue로 값을 꺼내고 isBlank로 value가 비었는지 확인 비었으면 에러
-				// 스페이스바를 빈값으로 본다
-				response.put("error", userEntry.getKey() + "은(는) 공백일 수 없습니다."); // 에러 날려줌 어느필드가 비었는지 알기위해 entry사용
+
+			if (userEntry.getValue().isBlank()) {
+				response.put("error", userEntry.getKey() + "은(는) 공백일 수 없습니다.");
 				return response;
-
 			}
-
 		}
 
 		User user = gson.fromJson(userJson, User.class);
@@ -57,82 +56,64 @@ public class UserService {
 		}
 
 		if (duplicatedEmail(user.getEmail())) {
-			response.put("error", "이미 가입된 이메일입니다.");
+			response.put("error", "이미 가입된 이메일 입니다.");
 			return response;
 		}
-//		String pw = BCrypt.hashpw("1234", BCrypt.gensalt());
-//		System.out.println(pw); 
-//		System.out.println(BCrypt.checkpw("1234", pw)); 
-//		
-//		
-		// user.setPassword(BCrypt.);
 
 		user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
 		System.out.println("암호화 후");
 		System.out.println(user);
 
 		userRepository.saveUser(user);
-		
-		RoleDtl roldDtl = RoleDtl.builder()
-				.roleId(3)
-				.userId(user.getUserId())
-				
-				.build();
-		
-	
 
-		response.put("ok", "회원가입 성공");
+		RoleDtl roleDtl = RoleDtl.builder().roleId(3).userId(user.getUserId()).build();
+
+		userRepository.saveRoleDtl(roleDtl);
+
+		response.put("ok", "회원가입 성공.");
 
 		return response;
 	}
-//	public void register(String userJson) {
-//		User user = gson.fromJson(userJson, User.class);
-//		if(duplicatedUsername(user.getUsername())) {  //notnull이라는 뜻
-//			
-//		}
 
 	private boolean duplicatedUsername(String username) {
 		return userRepository.findUserByUsername(username) != null;
-
 	}
 
 	private boolean duplicatedEmail(String email) {
 		return userRepository.findUserByEmail(email) != null;
-
 	}
 
-	public Map<String, String> authorize(String loginUserJson) {  //밑의 if가 다 안걸리면 빨간줄
-		Map<String, String> loginUser = gson.fromJson(loginUserJson, Map.class); // 이 json을 맵으로 바꿈
-		
+	public Map<String, String> authorize(String loginUserJson) {
+		Map<String, String> loginUser = gson.fromJson(loginUserJson, Map.class);
+
 		Map<String, String> response = new HashMap<>();
-		
-		for(Entry<String, String> entry : loginUser.entrySet()) {
-			if(entry.getValue().isBlank()) {
+
+		for (Entry<String, String> entry : loginUser.entrySet()) {
+			if (entry.getValue().isBlank()) {
 				response.put("error", entry.getKey() + "은(는) 공백일 수 없습니다.");
 				return response;
 			}
 		}
-		
+
 		String usernameAndEmail = loginUser.get("usernameAndEmail");
-		
-		User user = userRepository.findUserByEmail(loginUser.get("usernameAndEmail"));
-		if(user == null) {
+
+		User user = userRepository.findUserByUsername(usernameAndEmail);
+		if (user == null) {
 			user = userRepository.findUserByEmail(usernameAndEmail);
-			if(user == null) {
+			if (user == null) {
 				response.put("error", "사용자 정보를 확인해주세요.");
 				return response;
 			}
 		}
-		//아이디는 맞췄다
-		if(!BCrypt.checkpw(loginUser.get("password"), user.getPassword())) {
+
+		if (!BCrypt.checkpw(loginUser.get("password"), user.getPassword())) {
 			response.put("error", "사용자 정보를 확인해주세요.");
 			return response;
 		}
-		
-		response.put("ok", user.getName() +  "님 환영합니다.");
+
+		response.put("ok", user.getName() + "님 환영합니다.");
 		return response;
-		
-		
+
 	}
 
 }
