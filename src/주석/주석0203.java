39ba3bd959596183a464307b8a1ecae405061824
smj0104 <hikoseki1997@naver.com>
2023-02-03 17:47:52 +0900

    0203 python Day04 정리

---
+++ src/주석/주석0203.java
@@ -0,0 +1,36 @@
+package 주석;
+
+public class 주석0203 {
+/*
+ * mysql
+ * 
+ * trigger 설명 부가
+ * trigger = 일정한 조건이 만족되면 자동으로 실행  프로시져나 펑션과는 다르게 한번 설정하면 대기하다가 특정 조건을 만족하는 순간 실행
+ * 본인이 직접 실행불가, 매개변수 사용불가, mysql은 view에 트리거 사용불가
+ * 
+ * 
+ *  		DELIMITER $$
+	CREATE TRIGGER trigger_name
+	{BEFORE | AFTER} {INSERT | UPDATE| DELETE }
+	ON table_name FOR EACH ROW
+	BEGIN
+		-- 트리거 내용
+	END
+DELIMITER ;
+	BEGIN과 END 사이에
+	
+	
+	
+	foreign key
+	외래키라고 불리며 테이블을 다른 테이블과연결해주는 역할을 한다 (하나의 테이블을 다른 테이블에 의존시킴)
+	외래키 제약 조건 설정시 참조되는 테이블의 필드는 unique 나 primary key의 조건이 붙어야한다
+	REFERENCES 뒤에 붙은 테이블을 참조함
+	
+	
+	서브쿼리
+	다른 쿼리 내부에 있는 select문  반드시 괄호로 감싸야함
+	
+
+
+ */
+}
