9e65b87ec815c6e9f17674819eaf8ecb825ec08d
smj0104 <hikoseki1997@naver.com>
2023-02-17 17:52:47 +0900

    파이선 정리 0217

--- src/주석/주석0215.java
+++ src/주석/주석0215.java
@@ -1,5 +1,9 @@
 package 주석;
 
+import java.util.List;
+
+import javax.swing.JTextArea;
+
 public class 주석0215 {
 /* Socket project
  * 방 나갈때 회원 나감 표시와 이전 화면으로 이동 
@@ -8,6 +12,9 @@ public class 주석0215 {
  * 화면 비율 가로 480 세로 800
  * 클라 / 서버 하루마다 돌아가면서 
  * 각각의 프로젝트 마다 main문이 하나만 있어야한다
+ * 
+ * 
+ * 한번에 해결하려하지말고 하나씩 천천히 할것
  * 
  * 클라dto 서버dto 따로
  * 이미지 사이즈 맞추는 법 찾기
@@ -21,5 +28,20 @@ public class 주석0215 {
  * 
  * 카카오 아이콘
  * https://developers.kakao.com/
+ * 
+ * https://thsd-stjd.tistory.com/147 참고
+ * https://m.blog.naver.com/simhs93/221980987519
+ * https://myhappyman.tistory.com/103
+ * 
+ * 
+ * 
+ * private void clearFields(	List<JTextArea> textArea) {
+		for(JTextArea field : textArea) {
+			if(field.getText().isEmpty()) {
+				continue;
+			}
+			field.setText("");
+		}
+	}
  */
 }
