983a849c45ec472bb6a6345133c0f48cbe9c26d1
smj0104 <hikoseki1997@naver.com>
2023-02-21 17:47:26 +0900

    0221 python 정리

--- src/주석/주석0215.java
+++ src/주석/주석0215.java
@@ -13,6 +13,7 @@ public class 주석0215 {
  * 클라 / 서버 하루마다 돌아가면서 
  * 각각의 프로젝트 마다 main문이 하나만 있어야한다
  * 
+ * https://lcodea.tistory.com/22 소켓채팅 참고
  * 
  * 한번에 해결하려하지말고 하나씩 천천히 할것
  * 
@@ -40,6 +41,42 @@ public class 주석0215 {
 				continue;
 			}
 			field.setText("");
+		}
+	}
+	클라 리시브
+	responseDto.getUserId() + ": " +  추가
+	
+	
+	
+
+소켓서버
+  case "sendMessage":
+	            String message = (String) requestDto.getBody();
+	            enterRoomName = (String) requestDto.getEnterRoomname();
+	            String username1 = (String)requestDto.getUsername();
+	            
+	            ResponseDto<?> messageResponseDto = ResponseDto.<String>builder()
+	            												.resource("sendMessage")
+														.body(message)							
+	            												.room(room)
+	            												.username(username)
+	            												.roomName(roomName)
+	            												.build();
+	            sendToRoom(messageResponseDto,roomName);
+	            break;
+	            
+	         클라챗   
+	private void sendMessage() {
+		if (!messageInput.getText().isBlank()) {
+			RequestDto<?> messageReqDto = RequestDto.<String>builder()
+													.resource("sendMessage")
+													.username(username)
+													.enterRoomname(enterRoomname)
+													.body(messageInput.getText())
+													.build();
+			sendRequest(messageReqDto);
+			messageInput.setText("");
+
 		}
 	}
  */
--- src/주석/주석0220.java
+++
@@ -1,5 +0,0 @@
-package 주석;
-
-public class 주석0220 {
-
-}
