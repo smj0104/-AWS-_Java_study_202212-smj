10d103e2f145786e126eacdfc9b59e8fd7291af6
smj0104 <hikoseki1997@naver.com>
2023-04-13 19:27:04 +0900

    0413 클래스 배열 보충

--- src/j12_배열/Array1.java
+++ src/j12_배열/Array1.java
@@ -27,4 +27,12 @@ public class Array1 {
 			System.out.println(nums[i]);
 		}
 	}
+	/**
+	 * 배열 = 일정한 크기로 이동할 수 있게끔 만듬 //클래스가 자료형이라 배열로 만들수있다.
+	 * 반복문이 중요/ .length나 forEach()
+	 * 
+	 * a [0, 1, 2, 3, 4]
+	 * for와forEach의 차이 인덱스의 유무, forEach는 새로운 공간에 값을 넣어서 가지고 옴 for는 바로 접근함
+	 * forEach = 메소드명(customer? 람다를 쓸수있는 인터페이스)
+	 */
 }
